5be95d6d00f2610b4d64319a7265ececdf06b199
Alexey <alexeylychagin@gmail.com>
2023-05-19 18:35:48 +0300

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ const svg = () => {
 }
 
 const sprite = () => {
-  return gulp.src('source/img/**/*.svg')
+  return gulp.src('source/img/icons/*.svg')
   .pipe(svgo())
   .pipe(svgstore({
     InlineSvg: true
