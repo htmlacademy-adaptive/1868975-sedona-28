03e1ba048005ae0b5ac81eba8b5b5c69f0649e47
Alexey <alexeylychagin@gmail.com>
2023-05-23 23:35:36 +0300

    Правки ч1

--- source/js/script.js
+++ source/js/script.js
@@ -2,18 +2,18 @@ let navMain = document.querySelector('.main-nav__menu');
 let navToggle = document.querySelector('.main-nav__toggle');
 let navClosetoggle = document.querySelector('.main-nav__close-toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav__menu--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__menu--closed')) {
+    navMain.classList.remove('main-nav__menu--closed');
+    navMain.classList.add('main-nav__menu--opened');
   }
 });
 
 navClosetoggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--opened')) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
+  if (navMain.classList.contains('main-nav__menu--opened')) {
+    navMain.classList.remove('main-nav__menu--opened');
+    navMain.classList.add('main-nav__menu--closed');
   }
 });
